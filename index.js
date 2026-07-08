eba2a3303969127cf0f1e2787d579e1506349ce4
ashuu998 <bhardwaj01aashu.gmail.com>
2026-07-08 21:42:18 +0530

    commented

--- index.js
+++ index.js
@@ -21,17 +21,17 @@ mongoose
   });
   app.use(slowdown);
 
-  const limiter = rateLimit({
-	windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-	// store: ... , // Redis, Memcached, etc. See below.
-})
+//   const limiter = rateLimit({
+// 	windowMs: 15 * 60 * 1000, // 15 minutes
+// 	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+// 	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+// 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+// 	ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+// 	// store: ... , // Redis, Memcached, etc. See below.
+// })
 
-// Apply the rate limiting middleware to all requests.
-app.use(limiter)
+// // Apply the rate limiting middleware to all requests.
+// app.use(limiter)
 app.use('/user', authRoutes);
 app.use('/customer',customerRoute);
   
